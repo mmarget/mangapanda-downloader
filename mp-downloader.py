1151282cf99c6a47d919093bf6cbd6e3d003b732
Max Marget <mmarget@physik.tu-berlin.de>
2017-06-14 16:56:20 +0200

    added progress bar

--- mp-downloader.py
+++ mp-downloader.py
@@ -4,6 +4,7 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import sys
 
 homepage = "http://www.mangapanda.com/"
 
@@ -14,19 +15,32 @@ manga = "dragon-ball"
 min_chapter = 1
 max_chapter = 520
 filepath = "./" + manga + "/"
+
 #create subfolder for images
 if not os.path.exists(filepath):
     os.makedirs(filepath)
 
 for chap in (range(min_chapter, max_chapter + 1)):
+
     #check for length of chapter
     contents = BeautifulSoup(requests.get(homepage + manga + "/" + str(chap)).content, 'html.parser')
-    print("Downloading chapter", chap, "of", max_chapter, "...")
-    for page in range(1, len(contents.find_all("option")) + 1):
-        #get image url from html
+    pages = len(contents.find_all("option"))
+
+    if pages == 0: 
+        print("Chapter " + str(chap) + " not found, skipping..")i
+
+    for page in range(1, pages + 1):
+        #neat progress bar
+        sys.stdout.write('\r')
+        sys.stdout.write("Downloading chapter %d of %d [%-50s] page %d/%d" % (chap, max_chapter, '='*int((page/pages)*50), page, pages))
+        sys.stdout.flush()
+
+        #get image url
         contents = BeautifulSoup(requests.get(homepage + manga + "/" + str(chap) + "/" + str(page)).content, 'html.parser')
         imgurl = contents.find(id="imgholder").find("img").get("src")
+
         #save to file
         f = open(filepath + manga + "-" + str(chap).zfill(4) + "-" + str(page).zfill(4) + ".jpg", 'wb')
         f.write(requests.get(imgurl).content)
         f.close()
+    sys.stdout.write('\n')
